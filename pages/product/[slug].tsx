4803ea4f1122274dcd90e1ac88dfa070716f80c7
Macbook <midolf.leesin@gmail.com>
2023-08-29 16:55:03 +0100

    update slug

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,4 +1,29 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
+import React, { ReactNode } from 'react';
+
+interface Product {
+  price: ReactNode;
+  description: ReactNode;
+  title: string; // Add the 'title' property with its type
+}
+
+interface ProductPageProps {
+  product: Product;
+}
+
+const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
+  // Your page content here
+  return (
+    <div>
+      <h1>Product Page</h1>
+      {/* Display product details using the 'product' prop */}
+      <p>{product.title}</p>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      {/* Additional content */}
+    </div>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slug = params?.slug as string;
@@ -11,6 +36,16 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 };
 
-function fetchProductBySlug(slug: string) {
-    throw new Error('Function not implemented.');
+async function fetchProductBySlug(slug: string): Promise<Product> {
+  // Implement your data fetching logic here
+  // For example, fetch product data from an API or database
+  // Return the product data as an object
+  return {
+    title: 'Sample Product',
+    description: 'This is a sample product description.',
+    price: 19.99,
+    // Add other product details here
+  };
 }
+
+export default ProductPage;
