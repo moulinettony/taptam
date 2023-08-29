5122495d6745e71da606348eb1ac363c6c409480
Macbook <midolf.leesin@gmail.com>
2023-08-29 17:34:40 +0100

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,11 +8,11 @@ function Navbar() {
             <img src='/images/logo.svg' className="text-2xl font-bold"></img>
         </a>
         <div className="max-lg:hidden space-x-4 text-black">
-            <a href="#" className="hover:text-gray-400">Home</a>
-            <a href="#" className="hover:text-gray-400">About</a>
+            <a href="/" className="hover:text-gray-400">Home</a>
+            <a href="/productpage" className="hover:text-gray-400">Product</a>
             <a href="#" className="hover:text-gray-400">Contact</a>
         </div>
-        <a href="#">
+        <a href="/productpage">
             <img src='/images/card.svg' className="text-2xl font-bold"></img>
         </a>
     </nav>
